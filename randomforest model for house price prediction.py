968d45e7ead96ab49ea019c1a215b9a09afb8d5c
Muhammed Rahil CK <139740420+rahilmuhammedck@users.noreply.github.com>
2023-11-16 14:07:34 +0530

    update

--- randomforest model for house price prediction.py
+++ randomforest model for house price prediction.py
@@ -26,3 +26,9 @@ rf_val_predictions = rf_model.predict(test_X)
 rf_val_mae = mean_absolute_error(rf_val_predictions, test_y)
 
 print(" MAE for Random Forest Model: {:,.0f}".format(rf_val_mae))
+
+#The file after prediction
+
+pr_file = pd.DataFrame({'Id': train_data.Id, 'SalePrice' : rf_val_predictions})
+
+pr_file.to_csv('rf_model_predictions.csv', index=False)
